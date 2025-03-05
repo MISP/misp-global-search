247630f19e64c7e78251d08032fdb372e682d41a
NMD03 <nicjan01@gmail.com>
2025-03-05 13:46:03 +0100

    Add [website] search tips

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -298,3 +298,9 @@ document.addEventListener("DOMContentLoaded", () => {
     performSearch();
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl);
+    });
+});
